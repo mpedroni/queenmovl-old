696d2f3ec76fea1ddf63221caa242903e595c1cd
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-15 23:40:15 -0300

    create organisms components

---
+++ web/src/components/organisms/AppBar.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Logo from '@/components/atoms/Logo.vue';
+
+export default defineComponent({
+  name: 'AppBar',
+
+  components: {
+    Logo,
+  },
+});
+</script>
+
+<template>
+  <nav>
+    <Logo />
+  </nav>
+</template>
---
+++ web/src/components/organisms/FilmTable.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+
+import Film from '@/types/Film';
+
+import QTable from '@/components/molecules/Table.vue';
+
+export default defineComponent({
+  name: 'FilmTable',
+
+  components: {
+    QTable,
+  },
+
+  props: {
+    films: {
+      type: Array as PropType<Film[]>,
+      required: true,
+    },
+  },
+
+  setup() {
+    const headers = ref([
+      {
+        label: 'Nome',
+        property: 'name',
+      },
+      {
+        label: 'Sugerido por',
+        property: 'suggestedBy',
+      },
+      {
+        label: 'Plataforma',
+        property: 'plataform',
+      },
+      {
+        label: 'Nota',
+        property: 'grade',
+      },
+    ]);
+
+    return {
+      headers,
+    };
+  },
+});
+</script>
+
+<template>
+  <QTable
+    :headers="headers.map(header => header.label)"
+    :items="films.map(film => Object.values(film))"
+  />
+</template>
