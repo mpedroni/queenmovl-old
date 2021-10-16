aeb027671f409b4e2bf57049cf6d1b11ad0ca784
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-15 23:40:37 -0300

    create home template

---
+++ web/src/components/templates/_home.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+import Film from '@/types/Film';
+
+import AppBar from '@/components/organisms/AppBar.vue';
+import FilmTable from '@/components/organisms/FilmTable.vue';
+
+export default defineComponent({
+  name: '_home',
+
+  props: {
+    films: {
+      type: Array as PropType<Film[]>,
+      required: true,
+    },
+  },
+
+  components: {
+    AppBar,
+    FilmTable,
+  },
+});
+</script>
+
+<template>
+  <div id="container">
+    <AppBar />
+
+    <main>
+      <section id="film-table__section">
+        <FilmTable :films="films" />
+      </section>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  margin: 0 200px 0 200px;
+}
+
+#film-table__section {
+  display: flex;
+  justify-content: center;
+}
+</style>
