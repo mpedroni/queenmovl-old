8cd4ad1298e7c069c7e0b5a13ab24468f25c27c9
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-19 20:54:46 -0300

    create Btn atom and PrimaryBtn molecule

---
+++ web/src/components/atoms/Btn.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Btn',
+});
+</script>
+
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+@import url('../../css/colors.css');
+
+button {
+  width: fit-content;
+  max-width: 100%;
+  padding: 10px 20px;
+  border-radius: 8px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  height: 43px;
+  border: none;
+  transition: 0.2s;
+}
+
+button:hover {
+  cursor: pointer;
+  transform: translateY(-0.25em);
+  opacity: 0.9;
+}
+
+button:active {
+  opacity: 0.75;
+}
+</style>
---
+++ web/src/components/molecules/PrimaryBtn.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Btn from '@/components/atoms/Btn.vue';
+
+export default defineComponent({
+  name: 'PrimaryBtn',
+
+  components: {
+    Btn,
+  },
+});
+</script>
+
+<template>
+  <btn v-bind="$attrs">
+    <slot />
+  </btn>
+</template>
+
+<style scoped>
+@import url('../../css/colors.css');
+
+button {
+  background-color: var(--primary);
+  color: #fff;
+}
+
+button:hover {
+  box-shadow: 0 0.5em 0.5em -0.4em var(--primary);
+  background-color: var(--primary);
+}
+</style>
