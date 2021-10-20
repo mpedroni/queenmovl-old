70a52e6ea9cfaa16de8ca45afa2ab9ebe252de5f
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-20 17:06:49 -0300

    create "New Film" form dialog

---
+++ web/src/components/organisms/NewFilmDialog.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import QDialog from '@/components/atoms/Dialog.vue';
+
+export default defineComponent({
+  name: 'NewFilmDialog',
+
+  components: {
+    QDialog,
+  },
+
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<template>
+  <QDialog id="dialog">
+      <header id="toolbar">
+        <span id="toolbar-title">
+          Adicionar Filme
+        </span>
+      </header>
+  </QDialog>
+</template>
+
+<style scoped>
+@import url('../../css/colors.css');
+
+#dialog {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  border-radius: 12px;
+  width: 1000px;
+  height: 200px;
+}
+
+#dialog #toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--primary);
+  color: #fff;
+  padding: 15px;
+}
+
+#dialog #toolbar #toolbar-title {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
