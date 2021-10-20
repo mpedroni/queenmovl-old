45d95721f75efc6e2e529468f6a481f45ecb97b4
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-19 22:04:39 -0300

    add font-awesome

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import router from './router';
 import './css/global.css';
 
-createApp(App).use(router).mount('#app');
+library.add(faPlus);
+
+createApp(App).use(router).component('fa-icon', FontAwesomeIcon).mount('#app');
