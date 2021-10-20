a615be42dd7cdf3b7754a0d667decca41eeb20fb
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-20 17:04:21 -0300

    create Dialog atom

---
+++ web/src/components/atoms/Dialog.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'QDialog',
+
+  mounted() {
+    const dialog = window.document.querySelector('dialog');
+
+    /* eslint-disable-next-line */
+    // @ts-ignore
+    dialog.showModal();
+  },
+});
+</script>
+
+<template>
+  <dialog>
+    <slot />
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  border: none;
+  padding: 0;
+}
+
+dialog::backdrop {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+</style>
