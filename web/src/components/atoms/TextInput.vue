7a1994a705228faa58bc521d88937a16ffb1b6f2
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-24 22:08:52 -0300

    create the default text input (filled style)

--- web/src/components/atoms/TextInput.vue
+++ web/src/components/atoms/TextInput.vue
@@ -3,22 +3,81 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'TextInput',
+
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
 });
 </script>
 
 <template>
-  <input type="text">
+  <div class="input__wrapper">
+    <input id="text-input" type="text" v-bind="$attrs" placeholder="&nbsp;">
+    <label for="text-input">
+      {{ label }}
+    </label>
+  </div>
 </template>
 
 <style scoped>
-input {
-  padding: 5px 15px;
-  width: 50%;
-  height: 56px;
+@import url('../../styles/colors.css');
+
+* {
   box-sizing: border-box;
-  background-color: rgba(0, 0, 0, 0.2);
-  border-radius: 8px 8px 0 0;
+}
+
+.input__wrapper {
+  display: flex;
+  position: relative;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+}
+
+.input__wrapper label {
+  position: absolute;
+  left: 12px;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.5);
+  transition: all .2s ease 0s;
+  top: 20px;
+  pointer-events: none;
+}
+
+.input__wrapper input {
+  height: 56px;
+  padding: 16px 12px 0 12px;
+  font-size: 16px;
   border: none;
   outline: none;
+  background-color: rgba(0, 0, 0, 0.1);
+  border-radius: 8px 8px 0 0;
+  border-bottom: solid thin rgba(0, 0, 0, 0.5);
+  transition: all 0.2s ease;
+}
+
+.input__wrapper > input:hover {
+  background-color: rgba(0, 0, 0, 0.115);
+  border-bottom-color: rgba(0, 0, 0, 0.6);
+}
+
+.input__wrapper > input:focus {
+  border-bottom-color: var(--primary);
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.input__wrapper input:not(:placeholder-shown) + label {
+  top: 8px;
+  font-size: 13px;
+}
+
+.input__wrapper > input:focus + label {
+  font-size: 13px;
+  transition: all .2s ease 0s;
+  color: var(--primary);
+  top: 8px;
 }
 </style>
