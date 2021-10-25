c7ac001515a877645879c229a8a0de027a1071ed
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-25 18:02:43 -0300

    add error handling and type inference to response of search movie request

--- web/src/api/requests/searchMovie.ts
+++ web/src/api/requests/searchMovie.ts
@@ -1,12 +1,28 @@
+import axios, { AxiosError } from 'axios';
 import api from '@/api';
 
-interface SearchParams {
-  name: string
+import Movie from '@/types/Movie';
+
+interface SearchMovieParams {
+  name: string;
 }
 
-const searchMovie = async ({ name }: SearchParams): Promise<unknown> => {
+interface SearchMovieResponse {
+  page: number;
+  results: Movie[];
+  totalPages: number;
+  totalResults: []
+}
+
+interface SearchMovieError {
+  statusCode: number;
+  statusMessage: string;
+}
+
+const searchMovie = async (
+  { name }: SearchMovieParams): Promise<SearchMovieResponse | SearchMovieError> => {
   try {
-    const response = await api.get('/search/movie', {
+    const response = await api.get<SearchMovieResponse>('/search/movie', {
       params: {
         language: 'pt-BR',
         query: name,
@@ -15,8 +31,13 @@ const searchMovie = async ({ name }: SearchParams): Promise<unknown> => {
 
     return response.data;
   } catch (error) {
-    console.log(error);
-    return error;
+    if (axios.isAxiosError(error)) {
+      const searchMovieError = error as AxiosError<SearchMovieError>;
+
+      if (searchMovieError.response) return searchMovieError.response.data;
+    }
+
+    return { statusCode: 500, statusMessage: 'Ops Deu ruim' };
   }
 };
 
---
+++ web/src/types/Movie.ts
@@ -0,0 +1,10 @@
+export default interface Movie {
+  id: number;
+  originalLanguage: string;
+  originalTitle: string;
+  overview: string;
+  popularity: number;
+  posterPath: string;
+  releaseDate: string;
+  title: string;
+}
