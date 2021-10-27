1a8357956aa272289ddbac3c47d846992dc3eb28
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-27 20:51:28 -0300

    remove hardcoded test movies

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -11,38 +11,7 @@ export default defineComponent({
   },
 
   setup() {
-    const films = ref([
-      {
-        name: 'Euforia',
-        suggestedBy: 'Eduarda',
-        plataform: 'Netflix',
-        grade: 10,
-      },
-      {
-        name: 'O Mestre dos Genios',
-        suggestedBy: 'Matheus',
-        plataform: 'Prime Video',
-        grade: null,
-      },
-      {
-        name: 'Your Name',
-        suggestedBy: 'Eduarda',
-        plataform: 'Disney Plus',
-        grade: 8.5,
-      },
-      {
-        name: 'Beleza Oculta',
-        suggestedBy: 'Eduarda',
-        plataform: 'Netflix',
-        grade: 9,
-      },
-      {
-        name: 'Um Cadáver para Sobreviver',
-        suggestedBy: 'Matheus',
-        plataform: 'Netflix',
-        grade: 6,
-      },
-    ]);
+    const films = ref([]);
 
     return {
       films,
