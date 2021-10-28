6d62918b822d1c441cb33eb1fab9fef0b066fd9f
Matheus Pedroni <mpedroni77@gmail.com>
2021-10-28 12:36:27 -0300

    add style to user interaction in the MovieCard component

--- web/src/components/organisms/MovieCard.vue
+++ web/src/components/organisms/MovieCard.vue
@@ -16,28 +16,31 @@ export default defineComponent({
     formatDate(date: string) {
       return format(parseISO(date), 'LLLL/yyyy', { locale: ptBR })
     },
+    addMovie() {
+      console.log('add movie');
+    },
   },
 });
 </script>
 
 <template>
-  <div class="movie-card">
+  <div class="movie-card" @click="addMovie">
     <section class="poster">
       <img :src="`https://image.tmdb.org/t/p/w92/${movie.posterPath}`" :alt="`${movie.title} movie poster`">
     </section>
 
     <section class="main">
-      <h2>{{ movie.title }}</h2>
-      <h3>
+      <h2 @click.stop="() => {}">{{ movie.title }}</h2>
+      <h3 @click.stop="() => {}">
         {{ movie.originalTitle }}<span v-if="movie.originalTitle && movie.releaseDate">, </span>
         {{ movie.releaseDate && formatDate(movie.releaseDate) }}
       </h3>
 
-      <p>{{ movie.overview || 'Sinopse não encontrada' }}</p>
+      <p @click.stop="() => {}">{{ movie.overview || 'Sinopse não encontrada' }}</p>
     </section>
 
     <section class="infos">
-      <fa-icon :icon="{prefix: 'fas', iconName: 'chevron-right'}" />
+      <fa-icon class="icon" :icon="{prefix: 'fas', iconName: 'plus'}" />
     </section>
   </div>
 </template>
@@ -60,14 +63,22 @@ export default defineComponent({
   padding: 12px;
   border-radius: 8px;
   border: solid 1px rgba(0, 0, 0, 0.1);
+  transition: border 0.2s ease;
+  transition: background 0.2s ease;
 }
 
 .movie-card .poster img {
   border-radius: 8px;
 }
 
+.movie-card h2, h3, p {
+  cursor: text;
+  user-select: all;
+}
+
 .movie-card .main h2 {
   font-size: 18px;
+  transition: color 0.2s ease;
 }
 
 .movie-card .main h3 {
@@ -86,6 +97,22 @@ export default defineComponent({
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.movie-card .icon {
+  color: rgba(0, 0, 0, 0.3);
+  transition: border 0.2s ease;
+}
+
+.movie-card:hover .main h2,
+.movie-card:hover .infos .icon {
+  color: var(--primary);
+}
+
+.movie-card:hover {
+  border-color: var(--primary);
+  background-color: rgba(0, 0, 0, 0.05);
+  cursor: pointer;
 }
 
 </style>
