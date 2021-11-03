d184661dae0a78083e4c513774c1dc64eca483bb
Matheus Pedroni <mpedroni77@gmail.com>
2021-11-02 23:47:44 -0300

    create the login page

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -1,25 +1,26 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-
 import HomeTemplate from '@/components/templates/_home.vue';
+import LoginTemplate from '@/components/templates/_login.vue';
 
 export default defineComponent({
   name: 'Home',
-
   components: {
     HomeTemplate,
+    LoginTemplate,
   },
-
   setup() {
     const films = ref([]);
-
+    const user = ref(null);
     return {
       films,
+      user,
     };
   },
 });
 </script>
 
 <template>
-  <HomeTemplate :films="films" />
+  <HomeTemplate v-if="user" :films="films" />
+  <LoginTemplate v-else />
 </template>
