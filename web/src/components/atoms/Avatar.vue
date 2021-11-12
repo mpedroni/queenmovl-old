1e211100cc7a99f9d633f7743bb80520074297ea
Matheus Pedroni <mpedroni77@gmail.com>
2021-11-11 23:21:58 -0300

    create Avatar component

---
+++ web/src/components/atoms/Avatar.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Avatar',
+
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<template>
+  <img
+    :src="url"
+    v-bind="$attrs"
+    class="avatar"
+  />
+</template>
+
+<style scoped>
+.avatar {
+  width: 128px;
+  align-self: center;
+  cursor: pointer;
+}
+</style>
